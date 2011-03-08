fe98d38139eb8ab0f87ce2768c912e3a33a3de20
Will Morton <macavity@well.com>
2011-03-08 15:11:42 +0200

    Changed shell script

--- dist/template/tarball/robonobo.sh
+++ dist/template/tarball/robonobo.sh
@@ -7,11 +7,11 @@ JVMARGS=$MEMOPTS $DEBUG_LISTEN
 
 export CLASSPATH
 
-export cfg_robo_metadataServerUrl=http://robonobo.com:9080/midas/
-export cfg_robo_playlistUrlBase=http://robonobo.com:9080/website/p/
-export cfg_robo_sonarServerUrl=http://robonobo.com:9080/sonar/
-export cfg_robo_updateCheckUrl=http://robonobo.com:9080/website/checkupdate
-export cfg_robo_userAccountUrl=http://robonobo.com:9080/website/account
-export cfg_wang_bankUrl=http://robonobo.com:9080/wang/
+export cfg_robo_metadataServerUrl=http://midas.robonobo.com/
+export cfg_robo_playlistUrlBase=http://rbnb.co/p/
+export cfg_robo_sonarServerUrl=http://sonar.robonobo.com/
+export cfg_robo_updateCheckUrl=http://robonobo.com/checkupdate
+export cfg_robo_userAccountUrl=http://robonobo.com/account
+export cfg_wang_bankUrl=http://bank.acceptwang.com/
 
 java $JVMARGS com.robonobo.Robonobo
