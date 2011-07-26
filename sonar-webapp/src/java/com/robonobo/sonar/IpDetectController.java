1a9182ea6b0e60e9dbe170760c93efb3f27e96ed
Will Morton <macavity@well.com>
2011-07-26 16:33:40 +0200

    Fixed IP detect in sonar when behind varnish

--- sonar-webapp/src/java/com/robonobo/sonar/IpDetectController.java
+++ sonar-webapp/src/java/com/robonobo/sonar/IpDetectController.java
@@ -5,16 +5,30 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IpDetectController {
+	Log log = LogFactory.getLog(getClass());
+	
 	@RequestMapping("/ipdetect")
 	public void ipDetect(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		String ipAddr = req.getRemoteAddr();
 		resp.setStatus(HttpServletResponse.SC_OK);
 		resp.setContentType("text/plain");
-		resp.getWriter().print(ipAddr);
+		String clientIp = getClientIpAddress(req);
+		log.info("Returning IP address to "+clientIp);
+		resp.getWriter().print(clientIp);
 	}
+	
+	/** If we're behind a front end cache, the client ip of the request won't be accurate - figure out the real ip */
+	protected String getClientIpAddress(HttpServletRequest req) {
+		String forwardFor = req.getHeader("X-Forwarded-For");
+		if(forwardFor == null)
+			return req.getRemoteAddr();
+		return forwardFor;
+	}
+
 }
